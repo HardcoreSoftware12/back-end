c94215291d90885b22a513161599377df13137ff
cipher <devadigaranjith12@gmail.com>
2024-02-29 17:56:28 +0530

    .

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose")
 
 const connect = ()=>{
     const username = process.env.DB_USER;
-const password = process.env.DB_PASSWORD
+    const password = process.env.DB_PASSWORD
 
 const uri = `mongodb+srv://${username}:${password}@cluster0.uuxrciw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
