5ded1a7e26d2fe984c141f4e2fbf2d7706eb37fc
cipher <devadigaranjith12@gmail.com>
2024-03-04 14:53:18 +0530

    ....

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser());
 
+
+
+
 app.use("/user",userRouter)
 app.use("/notes",notesRouter)
 
@@ -21,9 +24,7 @@ app.use("/notes",notesRouter)
 
 connect(process.env.DB_USER, process.env.DB_PASSWORD);
 
-app.get("/",(req,res)=>{
-    res.send("api running")
-})
+
 
 app.listen(PORT,()=>{
     console.log("server started at port",PORT);
